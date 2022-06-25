e7e414b7966a3fc8d1b2fd2fde3305136acb175a
jamil <jamil.cse13@gmail.com>
2022-06-25 11:08:34 +0600

    initial commit

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+@app.route("/index")
+@app.route("/home")
+def index():
+    return render_template("index.html", index=True)
